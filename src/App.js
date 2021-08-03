1ec0c43ea830939a5c951768ca16b401e9bcdd88
四方 秀 <shu.42111227@gmail.com>
2021-08-03 16:16:04 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Emotion } from "./components/Emotion";
+import { StyledComponents } from "./components/StyledComponents";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <StyledComponents />
+      <Emotion />
+    </div>
+  );
+}
